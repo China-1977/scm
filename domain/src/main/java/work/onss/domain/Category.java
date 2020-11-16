6dca82d1984df76ced764238149e028dc035cab2
wangchanghao <3166944856@qq.com>
2020-11-16 15:31:24 +0800

    .

---
+++ domain/src/main/java/work/onss/domain/Category.java
@@ -0,0 +1,12 @@
+package work.onss.domain;
+
+import java.util.List;
+
+public class Category {
+
+    private String id;
+    private String value;
+    private String label;
+    private Boolean disabled;
+    private List<Category> children;
+}
