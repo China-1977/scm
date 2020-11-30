a097a23d0695f57e6bd5057c332f0d629bcb957b
wangchanghao <3166944856@qq.com>
2020-11-30 18:49:16 +0800

    .

--- domain/src/main/java/work/onss/enums/OrderTypeEnum.java
+++ domain/src/main/java/work/onss/enums/OrderTypeEnum.java
@@ -12,8 +12,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 public enum OrderTypeEnum implements Serializable {
     PURCHASE(0, "采购订单"),
-    PRODUCE(1, "生产订单"),
-    OUTSOURCE(2, "委外订单");
+    PRODUCE(1, "生产订单");
     private final Integer code;
     private final String value;
 
--- domain/src/main/java/work/onss/enums/StoreStateEnum.java
+++ domain/src/main/java/work/onss/enums/StoreStateEnum.java
@@ -8,13 +8,14 @@ import java.io.Serializable;
 @Getter
 @AllArgsConstructor
 public enum  StoreStateEnum implements Serializable {
-
-    REJECTED(0,"审核失败"),
-    FINISHED(1,"入住成功"),
-    CANCELED(2,"撤销申请"),
-    EDITTING(3,"商户编辑中"),
-    SYSTEM_AUDITING(4,"平台审核中");
-
+    EDITING(0, "编辑中"),
+    AUDITING(1, "审核中"),
+    REJECTED(2, "已驳回"),
+    TO_BE_CONFIRMED(3, "待账户验证"),
+    TO_BE_SIGNED(4, "待签约"),
+    SIGNING(5, "开通权限中"),
+    FINISHED(6, "已完成"),
+    CANCELED(7, "已作废");
     private final Integer code;
     private final String value;
 
