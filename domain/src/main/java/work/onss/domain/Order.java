e605e49d97a1de162b1c5bb1ae59ca5e705002e0
wangchanghao <3166944856@qq.com>
2020-11-30 17:55:11 +0800

    .

--- domain/src/main/java/work/onss/domain/Order.java
+++ domain/src/main/java/work/onss/domain/Order.java
@@ -1,6 +1,7 @@
 package work.onss.domain;
 
 import lombok.Data;
+import work.onss.enums.OrderStatusEnum;
 
 import java.util.List;
 
@@ -10,5 +11,16 @@ import java.util.List;
 @Data
 public class Order {
     private String id;
+    /**
+     * 订单商户
+     */
+    private Merchant merchant;
+    /**
+     * 订单清单
+     */
     private List<Product> products;
+    /**
+     * 订单状态
+     */
+    private OrderStatusEnum status;
 }
