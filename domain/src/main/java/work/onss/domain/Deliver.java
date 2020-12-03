5a0f94238420e38f3274b7330535ce4ee893fe4e
wangchanghao <3166944856@qq.com>
2020-12-03 17:48:25 +0800

    .

--- domain/src/main/java/work/onss/domain/Deliver.java
+++ domain/src/main/java/work/onss/domain/Deliver.java
@@ -3,6 +3,7 @@ package work.onss.domain;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,19 +16,80 @@ public class Deliver implements Serializable {
      */
     private String id;
     /**
-     * 订单交付编号
-     */
-    private String code;
-    /**
      * 商户ID
      */
     private String mid;
     /**
+     * 订单交付编号
+     */
+    private String code;
+    /**
      * 订单交付清单
      */
-    List<Product> products;
+    List<Cargo> cargos;
     /**
      * 物料信息
      */
     private Logistic logistic;
+
+    @Data
+    private static class Cargo implements Serializable{
+        /**
+         * 产品ID
+         */
+        private String id;
+        /**
+         * 计划ID
+         */
+        private String planId;
+        /**
+         * 计划编号
+         */
+        private String planCode;
+        /**
+         * 订单ID
+         */
+        private String orderId;
+        /**
+         * 订单编号
+         */
+        private String orderCode;
+        /**
+         * 合同编号
+         */
+        private String contractCode;
+        /**
+         * 产品名称
+         */
+        private String name;
+        /**
+         * 产品编号
+         */
+        private String number;
+        /**
+         * 产品质量
+         */
+        private BigDecimal weight;
+        /**
+         * 产品单价
+         */
+        private BigDecimal price;
+        /**
+         * 产品69码
+         */
+        private String barcode;
+        /**
+         * 产品分类
+         */
+        private Category category;
+        /**
+         * 产品子集
+         */
+        private List<Product> children;
+        /**
+         * 交付数量
+         */
+        private BigDecimal count;
+    }
+
 }
