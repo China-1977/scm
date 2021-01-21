5f3a6e0c02202e52cebc007987435687cd95822f
wangchanghao <3166944856@qq.com>
2021-01-21 10:00:34 +0800

    .

--- domain/src/main/java/work/onss/domain/Plan.java
+++ domain/src/main/java/work/onss/domain/Plan.java
@@ -4,44 +4,28 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * 计划
+ * 主键 计划编号 存货编码 存货名称 存货分类	单位 参考单价 计划数量 订单数量 入库数量 退回数量 计划日期 创建者 创建时间 更新时间 更新者
  */
 @Data
 public class Plan implements Serializable {
-    /**
-     * 计划ID
-     */
     private String id;
-    /**
-     * 计划编号
-     */
     private String code;
-    /**
-     * 计划数量
-     */
-    private BigDecimal count;
-    /**
-     * 计划待入库订单数量
-     */
-    private BigDecimal orderCount;
-    /**
-     * 计划已入库数量
-     */
-    private BigDecimal inCount;
-    /**
-     * 计划产品
-     */
-    private List<Product> products;
-    /**
-     * 计划创建时间
-     */
-    private LocalDateTime insertDateTime;
-    /**
-     * 计划更新时间
-     */
+    private String materialCode;
+    private String materialName;
+    private String materialUnit;
+    private BigDecimal materialPrice = BigDecimal.ZERO;
+    private BigDecimal quantity = BigDecimal.ZERO;
+    private BigDecimal orderQuantity = BigDecimal.ZERO;
+    private BigDecimal inQuantity = BigDecimal.ZERO;
+    private BigDecimal returnQuantity = BigDecimal.ZERO;
+    private LocalDate taskDate;
+    private String creator;
+    private String updater;
+    private LocalDateTime createDateTime;
     private LocalDateTime updateDateTime;
 }
