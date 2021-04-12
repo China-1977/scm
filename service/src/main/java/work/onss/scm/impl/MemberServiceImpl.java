3093494c78caf7c9f55b688444d7401451c3d242
邢力文 <313545922@qq.com>
2021-04-12 14:49:02 +0800

    会员新增

---
+++ service/src/main/java/work/onss/scm/impl/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package work.onss.scm.impl;
+
+import work.onss.scm.domain.Member;
+import work.onss.scm.service.MemberService;
+
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    public void addMember(Member member) {
+
+    }
+}
