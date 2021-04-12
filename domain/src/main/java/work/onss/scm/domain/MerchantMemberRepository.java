55c7052c01952626ef1db0277851659f13cb3126
wangchanghao <3166944856@qq.com>
2021-04-12 11:15:04 +0800

    .

---
+++ domain/src/main/java/work/onss/scm/domain/MerchantMemberRepository.java
@@ -0,0 +1,7 @@
+package work.onss.scm.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MerchantMemberRepository extends JpaRepository<MerchantMember,String> {
+}
