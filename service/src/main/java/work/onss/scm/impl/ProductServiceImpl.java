e9afa2b39064409f9d45dde629368fcd6b7f9c62
姜晓静 <xiaojing_jiang123@163.com>
2021-04-12 17:33:12 +0800

    产品信息接口添加

---
+++ service/src/main/java/work/onss/scm/impl/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package work.onss.scm.impl;
+
+import work.onss.scm.domain.Product;
+import work.onss.scm.service.ProductService;
+
+/**
+ * @author jiangxiaojing
+ * 产品信息实现
+ */
+public class ProductServiceImpl implements ProductService {
+
+    /**
+     * 根据产品编码获取产品信息
+     * @param code 产品编码
+     * @return 产品信息
+     */
+    @Override
+    public Product findProductByCode(String code) {
+        return null;
+    }
+}
---
+++ service/src/main/java/work/onss/scm/service/ProductService.java
@@ -0,0 +1,19 @@
+package work.onss.scm.service;
+
+import work.onss.scm.domain.Product;
+
+import java.util.List;
+
+/**
+ * @author jiangxiaojing
+ * 产品信息操作
+ */
+public interface ProductService {
+
+    /**
+     * 根据产品编码获取产品信息
+     * @param code 产品编码
+     * @return 产品信息
+     */
+    Product findProductByCode(String code);
+}
